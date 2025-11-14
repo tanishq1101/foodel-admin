3999464b5951eaea548aa2f4b4515177fad5b563
Tanishq <bazball06@gmail.com>
2025-11-14 21:23:11 +0530

    admin

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import ListRestaurants from "./pages/listRestaurant/listRestaurant.jsx";
 
 
 const App = () => {
-  const url = "https://fooddel-backend.vercel.app/";
+  const url = "https://fooddel-backend.vercel.app";
   return (
     <div>
       <ToastContainer />
